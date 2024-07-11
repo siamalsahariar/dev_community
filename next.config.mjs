128c4ebfdf7fc1d8707eca77687f21b11fafa98f
Abstract Programmer <AbstractProgrammer@example.com>
2024-07-11 23:52:45 +0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
+    
+  
+  }
+    
+  
+  export default nextConfig;
+  
